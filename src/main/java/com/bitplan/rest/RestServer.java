4565f08c7b6fa97e59e02f2d87cfc0756e5fb18a
Wolfgang Fahl <wf@bitplan.com>
2016-06-12 07:10:15 +0200

    refactors to find available port for test

--- src/main/java/com/bitplan/rest/RestServer.java
+++ src/main/java/com/bitplan/rest/RestServer.java
@@ -1,65 +1,74 @@
 package com.bitplan.rest;
 
 /**
- * Rest Server Interface (without Guice) 
+ * Rest Server Interface (without Guice)
+ * 
  * @author wf
  *
  */
 public interface RestServer {
 
-	/**
-	 * set the Guice Module to be used for dependency injection
-	 * 
-	 * @param pGuiceModule
-	 */
-	public abstract Object getStarter();
+  /**
+   * set the Guice Module to be used for dependency injection
+   * 
+   * @param pGuiceModule
+   */
+  public abstract Object getStarter();
 
-	public abstract void setStarter(Object starter);
+  public abstract void setStarter(Object starter);
 
-	/**
-	 * @return the settings
-	 */
-	public abstract RestServerSettings getSettings();
+  /**
+   * @return the settings
+   */
+  public abstract RestServerSettings getSettings();
 
-	/**
-	 * @param settings
-	 *          the settings to set
-	 */
-	public abstract void setSettings(RestServerSettings settings);
+  /**
+   * @param settings
+   *          the settings to set
+   */
+  public abstract void setSettings(RestServerSettings settings);
 
-	/**
-	 * stop
-	 */
-	public abstract void stop();
+  /**
+   * stop
+   */
+  public abstract void stop();
 
-	/**
-	 * get the base url
-	 * 
-	 * @return
-	 */
-	public abstract String getUrl();
+  /**
+   * get the base url
+   * 
+   * @return
+   */
+  public abstract String getUrl();
 
-	/**
-	 * start the WebServer
-	 * 
-	 * @see http
-	 *      ://jersey.576304.n2.nabble.com/Embedded-Grizzly-with-MVC-templates
-	 *      -td4189027.html for SSL security
-	 * 
-	 * @throws Exception
-	 */
-	public abstract void startWebServer() throws Exception;
+  /**
+   * start the WebServer
+   * 
+   * @see http
+   *      ://jersey.576304.n2.nabble.com/Embedded-Grizzly-with-MVC-templates
+   *      -td4189027.html for SSL security
+   * 
+   * @throws Exception
+   */
+  public abstract void startWebServer() throws Exception;
 
-	/**
-	 * run the server thread
-	 */
-	public abstract void run();
+  /**
+   * run the server thread
+   */
+  public abstract void run();
 
-	/**
-	 * start server in separate thread
-	 * 
-	 * @param args
-	 */
-	public abstract RestServer startServer(String[] args);
+  /**
+   * start server in separate thread
+   * 
+   * @param args
+   */
+  public abstract RestServer startServer(String[] args);
+
+  /**
+   * check whether the given port is available
+   * 
+   * @param port
+   * @return
+   */
+  public boolean isPortAvailable(int port);
 
 }
